1d3b5d6e28fef9abc5d4d930891b60bf9d71b95b
liukeyuan <492465798@qq.com>
2023-03-08 18:03:21 +0800

    更新log多版本和可配置

--- log/cfg.go
+++ log/cfg.go
@@ -1,7 +1,13 @@
 package log
 
+import formatter "github.com/antonfisher/nested-logrus-formatter"
+
 type Config struct {
-	Path         string `json:"path" yaml:"path"`
-	MaxAge       int64  `json:"max_age" yaml:"maxAge"`
-	RotationTime int64  `json:"rotation_time" yaml:"rotationTime"`
+	Name                  string               `json:"name" yaml:"name"`                                      //日志名称
+	Path                  string               `json:"path" yaml:"path"`                                      // 日志路径
+	MaxAge                int64                `json:"max_age" yaml:"maxAge"`                                 // 日志文件最多保存时间
+	RotationTime          int64                `json:"rotation_time" yaml:"rotationTime"`                     //多久新建一个日志文件
+	EnableSetReportCaller bool                 `json:"enable_set_report_caller" yaml:"enableSetReportCaller"` // 是否打印调用链
+	Level                 int                  `json:"level" yaml:"level"`                                    //日志级别
+	Formatter             *formatter.Formatter `json:"formatter" yaml:"formatter"`                            //格式设置
 }
--- log/log.go
+++ log/log.go
@@ -1,28 +1,51 @@
 package log
 
 import (
-	"fmt"
-	nested "github.com/antonfisher/nested-logrus-formatter"
 	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
 	"github.com/sirupsen/logrus"
 	"io"
+	"log"
 	"os"
-	"path"
-	"runtime"
 	"sync"
 	"time"
 )
 
 var (
-	logger *logrus.Logger
-	once   sync.Once
+	logMap sync.Map
 )
 
 func Api(cfg *Config) *logrus.Logger {
-	once.Do(func() {
-		// 设置日志格式为json格式
-		logger = logrus.New()
-		logger.SetFormatter(&nested.Formatter{
+
+	value, ok := logMap.Load(cfg.Name)
+	if ok {
+		return value.(*logrus.Logger)
+	}
+
+	// 设置日志格式为json格式
+	logger := logrus.New()
+	logger.SetFormatter(cfg.Formatter)
+	logger.SetReportCaller(cfg.EnableSetReportCaller)
+	logger.SetLevel(logrus.Level(cfg.Level))
+
+	writer, err := rotatelogs.New(
+		cfg.Path+".%Y%m%d%H%M",
+		rotatelogs.WithLinkName(cfg.Path),
+		rotatelogs.WithMaxAge(time.Second*time.Duration(cfg.MaxAge)),
+		rotatelogs.WithRotationTime(time.Second*time.Duration(cfg.RotationTime)),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fileAndStdoutWriter := io.MultiWriter(os.Stdout, writer)
+	logger.SetOutput(fileAndStdoutWriter)
+	logMap.Store(cfg.Name, logger)
+
+	return logger
+}
+
+/**
+Example:
+logger.SetFormatter(&nested.Formatter{
 			HideKeys:        true,
 			TimestampFormat: "[2006-01-02 15:04:05]",
 			CallerFirst:     true,
@@ -30,18 +53,4 @@ func Api(cfg *Config) *logrus.Logger {
 				return fmt.Sprintf("[%s:%d]", path.Base(f.File), f.Line)
 			},
 		})
-		logger.SetReportCaller(true)
-		logger.SetLevel(6)
-
-		writer, _ := rotatelogs.New(
-			cfg.Path+".%Y%m%d%H%M",
-			rotatelogs.WithLinkName(cfg.Path),
-			rotatelogs.WithMaxAge(time.Second*time.Duration(cfg.MaxAge)),
-			rotatelogs.WithRotationTime(time.Second*time.Duration(cfg.RotationTime)),
-		)
-
-		fileAndStdoutWriter := io.MultiWriter(os.Stdout, writer)
-		logger.SetOutput(fileAndStdoutWriter)
-	})
-	return logger
-}
+*/
